ce495e3fe4165f9dd13b03c2810c33c9c3047f27
Tunn <wolf123cpr@gmail.com>
2024-09-19 21:04:52 +0700

    Project ReactJS First

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import './App.css'
+import { useState } from "react";
+import data from './data.json'
+function App(props) {
+  const [todoList, setTodoList] = useState(data);
+  const toggleChecked = (index) => {
+    setTodoList([...todoList], todoList[index].status = !todoList[index].status)
+  }
+  const addTodo = () => {
+    const valueInput = document.getElementById("myInput").value;
+    setTodoList([...todoList, data.push({ name: valueInput, status: false })])
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div id="myDIV" className="header">
+        <h2 style={{ margin: "5px" }}>{props.myTitle}</h2>
+        <input type="text" id="myInput" placeholder="Title..." />
+        <span onClick={addTodo} className="addBtn">Add</span>
+      </div>
+      <ul id="myUL">
+        {
+          data.map((e, index) =>
+            <li onClick={() => toggleChecked(index)} className={e.status?"":"checked"} key={index}>{e.name}</li>
+          )
+        }
+      </ul>
     </div>
   );
 }
